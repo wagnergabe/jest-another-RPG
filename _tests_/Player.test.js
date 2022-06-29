c1059083c3e85ab590fa24a63850b6929e097fc1
GSvenny <gabrielswagner7@gmail.com>
2022-06-29 18:50:05 -0500

    created player prototypes

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -16,3 +16,22 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player("Gabe")
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test("get inventory from player or returns false", () => {
+    const player = new Player ("Gabe");
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
